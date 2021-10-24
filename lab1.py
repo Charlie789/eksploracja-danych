bf5d1d933d1b0b86d72e2c71545887b0847412fb
Charlie <aplik987@gmail.com>
2021-10-24 18:56:16 +0200

    Zadanie 3

--- lab1.py
+++ lab1.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+from collections import Counter
+from github import Github
+from matplotlib import pyplot as plt
+from os import environ
+
 
 def zad1():
     df = pd.read_csv('dane/netflix_titles.csv', index_col='show_id')
@@ -26,12 +31,31 @@ def zad2():
     print(f'Ilość pustych wartości: {df.isna().sum().sum()}')
     print(f'Ilość pustych wartości, suma skumulowana:')
     print(empty)
+    print('\n')
 
 
 def zad3():
-    pass
+    git = Github(environ['GH_ACCESS'])
+    user = git.get_user('MikiKru')
+    repos = user.get_repos()
+    print('****** ZAD 3 ******')
+    print(f'Liczba projektów: {repos.totalCount}')
+    print('Trwa pobieranie języków z repozytorium...')
+    try:
+        langs_list = [list(repo.get_languages()) for repo in repos]
+    except KeyboardInterrupt:
+        return
+    langs_counter = Counter(langs[0] for langs in langs_list if len(langs) > 0)
+    print('Liczba projektów w danym języku:')
+    for element in langs_counter.items():
+        print(element)
+
+    fig = plt.figure(figsize=(10, 7))
+    plt.pie(langs_counter.values(), labels=langs_counter.keys())
+    plt.show()
 
 
 if __name__ == '__main__':
     zad1()
     zad2()
+    zad3()
