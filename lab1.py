2b01c645cb7dce4032be0021f105e8e2dba9e141
Karol Appel <aplik987@gmail.com>
2021-10-27 21:50:52 +0200

    dokończenie zad2

--- lab1.py
+++ lab1.py
@@ -18,19 +18,33 @@ def zad1():
 
 def zad2():
     df = pd.read_csv('dane/titanic.csv')
+    pd.set_option('display.max_columns', None)
 
     cum_sum = 0
     empty_dict = {}
     for column in df.columns:
         part_sum = df[column].isna().sum()
         cum_sum += part_sum
-        empty_dict[column] = [part_sum, cum_sum]
-    empty = pd.DataFrame.from_dict(empty_dict, orient='index', columns=['Ilość pustych', 'Suma skumulowana'])
+        empty_percentage = part_sum / len(df)
+        empty_dict[column] = [part_sum, cum_sum, empty_percentage]
+    empty = pd.DataFrame.from_dict(empty_dict, orient='index', columns=['Ilość pustych', 'Suma skumulowana', 'Procent pustych'])
 
     print('****** ZAD 2 ******')
+    print(df)
+    print(f'Ilość wczytanych wierszych: {len(df)}')
     print(f'Ilość pustych wartości: {df.isna().sum().sum()}')
     print(f'Ilość pustych wartości, suma skumulowana:')
     print(empty)
+
+    columns_to_delete = [column for column in df.columns if empty_dict[column][2] > 0.3]
+    print(f'Kolumny: "{columns_to_delete}" posiadają > 30% pustych rekordów, zostaną usunięte')
+    df.drop(columns=columns_to_delete, inplace=True)
+    print(df)
+
+    print('Zastępuje wartości "female" na 0 i "male" na 1')
+    df.replace(to_replace={'female': 0, 'male': 1}, inplace=True)
+    print(df)
+
     print('\n')
 
 
