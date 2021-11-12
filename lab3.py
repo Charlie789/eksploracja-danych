bb3523c50d32b108c8302834dd028e903097dc49
Karol Appel <aplik987@gmail.com>
2021-11-12 17:31:28 +0100

    Tozbicie zad1_2 na osobne zadania

--- lab3.py
+++ lab3.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 @exercise
-def zad1_2(clusters):
+def zad1():
     data_path = 'dane/Sales_Transactions_Dataset_Weekly.csv'
     if not path.isfile(data_path):
         print(
@@ -34,23 +34,27 @@ def zad1_2(clusters):
     processed_data = pca.fit_transform(scaled_features)
     processed_df = pd.DataFrame(data=processed_data, columns=[f'COMP{i}' for i in range(processed_data.shape[1])])
     print('Zmniejszono wymiarowość do 2 przy użyciu PCA')
+    return processed_df
 
+
+@exercise
+def zad2(clusters, data):
     kmeans = KMeans(n_clusters=clusters, init='k-means++', random_state=0)
-    processed_df_kmeans = kmeans.fit_predict(processed_df)
+    processed_df_kmeans = kmeans.fit_predict(data)
 
     markers = [(i, j, 0) for i in range(2, 10) for j in range(1, 3)]
 
-    # Dodanie poszczególnych klastrów
+    # Dodanie do wykresu poszczególnych klastrów
     for i in range(clusters):
         plt.scatter(
-            processed_df[processed_df_kmeans == i]['COMP0'],
-            processed_df[processed_df_kmeans == i]['COMP1'],
+            data[processed_df_kmeans == i]['COMP0'],
+            data[processed_df_kmeans == i]['COMP1'],
             s=20,
             c=[(random.random(), random.random(), random.random())],
             marker=markers[i],
             label=f'Skupienie {i}'
         )
-    # Dodanie centroidów
+    # Dodanie do wykresu centroidów
     plt.scatter(
         kmeans.cluster_centers_[:, 0],
         kmeans.cluster_centers_[:, 1],
@@ -59,7 +63,7 @@ def zad1_2(clusters):
         marker='*',
         label='Centroidy'
     )
-    plt.title('K-MEANS')
+    plt.title('K-MEANS++')
     plt.legend()
     plt.show()
 
@@ -76,4 +80,5 @@ def prepare_parser():
 if __name__ == '__main__':
     parser = prepare_parser()
     args = parser.parse_args()
-    zad1_2(args.clusters)
+    data = zad1()
+    zad2(args.clusters, data)
