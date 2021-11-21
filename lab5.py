f2b2a9b2f42f4a82b58dc8c4098f6ec44e282f2f
Charlie <aplik987@gmail.com>
2021-11-21 17:24:09 +0100

    zadanie 2 z lab5 - większość głosów

--- lab5.py
+++ lab5.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import numpy as np
 
 from os import path
+
+from sklearn import svm
+from sklearn.ensemble import VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 from helper_functions import exercise
 
@@ -12,16 +20,6 @@ x_train = None
 y_train = None
 x_test = None
 y_test = None
-
-y_pred_knn = None
-y_pred_svm = None
-y_pred_tree = None
-y_pred_forest = None
-
-model_knn = None
-model_svc = None
-model_tree = None
-model_forest = None
 
 
 @exercise
@@ -48,5 +46,45 @@ def zad1():
     print('Załadowane pomyślnie dane')
 
 
+@exercise
+def zad2():
+    knn = KNeighborsClassifier()
+    svc = svm.SVC(C=1, kernel='linear')
+    lr = LogisticRegression()
+
+    voting_ens = VotingClassifier(estimators=[('knn', knn), ('svc', svc), ('lr', lr)], voting='hard')
+
+    _generate_excel_report([knn, lr, svc, voting_ens], 'dane/ensemble_learning.xlsx')
+
+
+def _generate_excel_report(clasifiers_list, out_name):
+    output = {}
+    for clf in clasifiers_list:
+        print(f'Generowanie raportu dla {clf.__class__.__name__}...')
+        clf.fit(x_train, y_train)
+        y_pred = clf.predict(x_test)
+        report = classification_report(y_test, y_pred, output_dict=True)
+        validation = cross_validate(clf, x_train, y_train, cv=5)
+        average = np.average(validation["test_score"])
+        std_deviation = np.std(validation["test_score"])
+        output[clf.__class__.__name__] = [
+            average,
+            std_deviation,
+            accuracy_score(y_test, y_pred),
+            report['macro avg']['recall'],
+            report['macro avg']['f1-score'],
+            report['macro avg']['precision']
+        ]
+
+    output_df = pd.DataFrame.from_dict(
+        output,
+        orient='index',
+        columns=['cross-average', 'cross-std', 'ACC', 'Recall', 'F1-score', 'AUC']
+    )
+    output_df.to_excel(out_name)
+    print(f'Raport wygenerowany do pliku: {out_name}')
+
+
 if __name__ == '__main__':
     zad1()
+    zad2()
