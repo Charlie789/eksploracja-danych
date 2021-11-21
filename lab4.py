9735faf277c13eada39540347c34a88372730093
Charlie <aplik987@gmail.com>
2021-11-21 12:09:54 +0100

    Wydzelenie podfunkcji w zadaniach. Opracowanie kolejnych klasyfikatorów - svm, decision tree, random forest

--- lab4.py
+++ lab4.py
@@ -1,11 +1,18 @@
 import pandas as pd
+import numpy as np
 
 from os import path
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.tree import DecisionTreeClassifier
 
 from helper_functions import exercise
 
+import warnings
+warnings.filterwarnings("ignore")
 
 x_train = None
 y_train = None
@@ -13,7 +20,14 @@ x_test = None
 y_test = None
 
 y_pred_knn = None
+y_pred_svm = None
+y_pred_tree = None
+y_pred_forest = None
 
+model_knn = None
+model_svc = None
+model_tree = None
+model_forest = None
 
 @exercise
 def zad1():
@@ -40,21 +54,53 @@ def zad1():
 
 @exercise
 def zad2():
-    global y_pred_knn
+    global y_pred_knn, y_pred_svm, y_pred_tree, y_pred_forest
+    global model_knn, model_svc, model_tree, model_forest
+
+    # KNN
+    print("Przygotowywanie modelu KNN...")
     knn = KNeighborsClassifier()
-    knn_model = knn.fit(x_train, y_train.values.ravel())
-    y_pred_knn = knn_model.predict(x_test)
+    model_knn = knn.fit(x_train, y_train.values.ravel())
+    y_pred_knn = model_knn.predict(x_test)
+    print("Przygotowano model KNN")
+
+    # SVM
+    print("Przygotowywanie modelu SVM...")
+    svc = svm.SVC(C=1, kernel='linear')
+    model_svc = svc.fit(x_train, y_train.values.ravel())
+    y_pred_svm = model_svc.predict(x_test)
+    print("Przygotowano model SVM")
+
+    # Decision Tree
+    print("Przygotowywanie modelu Decision Tree...")
+    forest = DecisionTreeClassifier()
+    model_tree = forest.fit(x_train, y_train.values.ravel())
+    y_pred_tree = model_tree.predict(x_test)
+    print("Przygotowano model Decision Tree")
+
+    # Random Forest
+    print("Przygotowywanie modelu Random Forest...")
+    forest = RandomForestClassifier(n_jobs=-1)
+    model_forest = forest.fit(x_train, y_train.values.ravel())
+    y_pred_forest = model_forest.predict(x_test)
+    print("Przygotowano model Random Forest")
 
 
 @exercise
 def zad3():
-    def print_scores(predition_model):
+    def print_scores(predition_model, name):
+        print(f"--{name}--")
         print(f'Accuracy Score: {accuracy_score(y_test, predition_model)}')
         print(f'Classification Report:\n{classification_report(y_test, predition_model)}')
         print(f'Confusion Matrix:\n{confusion_matrix(y_test, predition_model)}')
+        print()
 
-    print("--KNN--")
-    print_scores(y_pred_knn)
+    print_scores(y_pred_knn, "KNN")
+    print_scores(y_pred_svm, "SVM")
+    print_scores(y_pred_tree, "Decision Tree")
+    print_scores(y_pred_forest, "Random Forest")
+
+
 
 
 if __name__ == '__main__':
